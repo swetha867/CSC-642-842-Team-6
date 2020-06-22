18db49b5dcff26f3e162c549d64a1a5470f97bae
Admin <admin@kevins-Air.local>
2020-06-22 13:47:05 -0700

    "test1"

--- snapdeliver/src/App.js
+++ snapdeliver/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Signup from "./pages/signup";
+import home from "./pages/home";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 
@@ -9,9 +10,11 @@ function App() {
   return (
     <div className="App">
       <Router>
-         <Link to="/signUp">Signup</Link>    
+         <Link to="/signUp">Signup</Link>
+         <Link to="/home"> home</Link>   
          <Switch>
              <Route path="/signUp" component={Signup} />
+             <Route path="/home" component={home} />
          </Switch>
       </Router>
     </div>
