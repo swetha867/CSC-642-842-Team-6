faf60d6bd16978d9c38e9fc75af99a4d7dda1407
swetha867 <60675967+swetha867@users.noreply.github.com>
2020-07-26 19:44:07 -0700

    added signup ,login pages and meging serina changes to master

--- snapdeliver/src/pages/signup.js
+++ snapdeliver/src/pages/signup.js
@@ -1,23 +1,75 @@
 import React from "react";
+import "../css/snapDeli.css"
 
 
 
 const Signup = ()=>{
 return(
-  <form>
-  <div class="form-group row">
-    <label for="staticEmail" class="col-sm-2 col-form-label">Email</label>
-    <div class="col-sm-10">
-      <input type="text" readonly class="form-control-plaintext" id="staticEmail" value="email@example.com"/>
-    </div>
-  </div>
-  <div class="form-group row">
-    <label for="inputPassword" class="col-sm-2 col-form-label">Password</label>
-    <div class="col-sm-10">
-      <input type="password" class="form-control" id="inputPassword" placeholder="Password"/>
-    </div>
-  </div>
-</form>
+  <div>
+  <nav class="navbar navbar-expand-lg navbar-light bgg-light">
+      <img class="snap-logo card-img-top" src={require(`../images/snaplogo.png`)} alt=""/>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+      </button>
+  
+      <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+          <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+
+              <li class="nav-item active">
+              <a class="nav-link homepage" href="/home">Home <span class="sr-only">(current)</span></a>
+              </li>
+
+
+          </ul>
+          
+      </div>
+
+      <div>
+          <ul>
+
+              <a href="/login"><button type="button" class="btn btn-primary button-space">Login</button></a>
+
+              <a href="/signup"><button type="button" class="btn btn-primary button-space" onClick="/signup">SignUp</button></a>
+
+          </ul>
+      </div>
+  </nav>
+<div class="form-signin">
+
+<h1 class="h3 mb-3 font-weight-normal">Sign Up</h1>
+<div class="form-group">
+<label for="inputEmail" style={{"float":"left","font-weight":"bold"}}>User Name<span style={{ "color": "red" }}>*</span></label>
+<input type="email" class="form-control" id="inputEmail"  placeholder="userName"></input>
+
+
+</div>
+<div class="form-group">
+<label for="inputEmail" style={{"float":"left","font-weight":"bold"}}>Email <span style={{ "color": "red" }}>*</span></label>
+<input type="email" class="form-control" id="inputEmail"  placeholder="Enter Email"></input>
+
+
+</div>
+<div class="form-group">
+<label for="inputPassword" style={{"float":"left","font-weight":"bold"}}>Phone Number<span style={{ "color": "red" }}>*</span></label>
+ <input type="password" placeholder="phoneNumber"  class="form-control" id="phoneNumber"  ></input>
+
+</div>
+<div class="form-group">
+<label for="inputPassword" style={{"float":"left","font-weight":"bold"}}>Password<span style={{ "color": "red" }}>*</span></label>
+ <input type="password" placeholder="password"  class="form-control" id="inputPassword"  ></input>
+
+</div>
+By Signing up you agree to our<a href="#" > Terms and Conditions</a>
+<br/><br/>
+
+<a href="/login"><button class="btn btn-lg btn-success btn-block" type="submit">Sign up</button></a>
+<br/>
+
+<div >
+</div>
+</div>
+</div>
+
 
 )
 
