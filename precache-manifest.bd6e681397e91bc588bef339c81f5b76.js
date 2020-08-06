4699f3265a035d7524834e6068b6e66e691908b5
swetha867 <60675967+swetha867@users.noreply.github.com>
2020-08-05 19:52:26 -0700

    Updates

---
+++ precache-manifest.bd6e681397e91bc588bef339c81f5b76.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5971dc8d52a740e15dc7491e7a64961f",
+    "url": "/CSC-642-842-Team-6/index.html"
+  },
+  {
+    "revision": "1a492a7970d851c5c0fc",
+    "url": "/CSC-642-842-Team-6/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "c02554bb7339a915b1f2",
+    "url": "/CSC-642-842-Team-6/static/css/main.9f387805.chunk.css"
+  },
+  {
+    "revision": "1a492a7970d851c5c0fc",
+    "url": "/CSC-642-842-Team-6/static/js/2.affaab58.chunk.js"
+  },
+  {
+    "revision": "031b62b957b4fb917cd0ec81e2c8bd7b",
+    "url": "/CSC-642-842-Team-6/static/js/2.affaab58.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c02554bb7339a915b1f2",
+    "url": "/CSC-642-842-Team-6/static/js/main.2d414181.chunk.js"
+  },
+  {
+    "revision": "c01bac5db653fd9162e1",
+    "url": "/CSC-642-842-Team-6/static/js/runtime-main.b4f2c29a.js"
+  },
+  {
+    "revision": "c59b807bf9dfa9265b66a3e908a05170",
+    "url": "/CSC-642-842-Team-6/static/media/coffee.c59b807b.jpg"
+  },
+  {
+    "revision": "7a32930e4f85f950ba39c51dcdd19172",
+    "url": "/CSC-642-842-Team-6/static/media/drink.7a32930e.jpg"
+  },
+  {
+    "revision": "abbb2ac8f45d03e151bc910c92bee8ed",
+    "url": "/CSC-642-842-Team-6/static/media/friedrice.abbb2ac8.jpg"
+  },
+  {
+    "revision": "b4438b0095faa0d9452d01f504ffa018",
+    "url": "/CSC-642-842-Team-6/static/media/homewallpaper.b4438b00.png"
+  },
+  {
+    "revision": "ad1a76ad14b5e6347eb86b4090c10bb3",
+    "url": "/CSC-642-842-Team-6/static/media/jangba.ad1a76ad.jpg"
+  },
+  {
+    "revision": "ae2a43b17ccc3d1a6919c4d6b142cbcf",
+    "url": "/CSC-642-842-Team-6/static/media/jangba1.ae2a43b1.jpg"
+  },
+  {
+    "revision": "17086e89493c486684ab410594262f51",
+    "url": "/CSC-642-842-Team-6/static/media/lavieenrose.17086e89.jpg"
+  },
+  {
+    "revision": "7cc2f64def0924548abd3111e51924b8",
+    "url": "/CSC-642-842-Team-6/static/media/login-background.7cc2f64d.png"
+  },
+  {
+    "revision": "06ad65cc16e60b3641b732c05aa1b17a",
+    "url": "/CSC-642-842-Team-6/static/media/pizza.06ad65cc.jpg"
+  },
+  {
+    "revision": "08468130f36990e375ec4a4a60eb6242",
+    "url": "/CSC-642-842-Team-6/static/media/pizzahurt.08468130.jpg"
+  },
+  {
+    "revision": "f84af199e0b26a0f3ff40fe3e03f2455",
+    "url": "/CSC-642-842-Team-6/static/media/sandwich.f84af199.jpg"
+  },
+  {
+    "revision": "b00f2f18968b2a4cab4fd5b6ced46245",
+    "url": "/CSC-642-842-Team-6/static/media/snaplogo.b00f2f18.png"
+  },
+  {
+    "revision": "7bb6924f389957f091074f5ab6c553c7",
+    "url": "/CSC-642-842-Team-6/static/media/taco.7bb6924f.jpg"
+  }
+]);
